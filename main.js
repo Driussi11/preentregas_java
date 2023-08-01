3796d5967db57566166ab2675a091a8bca13ecfb
Martin Sallies <martinsalliesopkk@gmail.com>
2023-08-01 12:13:53 -0300

    preentrega2

--- main.js
+++ main.js
@@ -1,39 +1,67 @@
-function correr(){
-    let nombre = prompt("Cual es su nombre?")
-    if (nombre == ""){
-        alert("Dale, pone un nombre");
-        return;
-    }
-    if (nombre == "Jorge")
-    alert("Ay, hola tutor c:")
+// function correr(){
+//     let nombre = prompt("Cual es su nombre?")
+//     if (nombre == ""){
+//         alert("Dale, pone un nombre");
+//         return;
+//     }
+//     if (nombre == "Jorge")
+//     alert("Ay, hola tutor c:")
 
-    if (nombre == "jorge")
-    alert("Ay, hola tutor c:")
+//     if (nombre == "jorge")
+//     alert("Ay, hola tutor c:")
 
 
-    let apellido = prompt("Cual es su apellido?")
-    if (apellido == ""){
-        alert("Eu, apellido");
-        return;
-    }
+//     let apellido = prompt("Cual es su apellido?")
+//     if (apellido == ""){
+//         alert("Eu, apellido");
+//         return;
+//     }
 
-    alert("Hola " + nombre + " " + apellido + "!");
+//     alert("Hola " + nombre + " " + apellido + "!");
 
-    let edad = prompt("Cuantos años tenes?")
-    if (edad>=18)
-        alert("Sos mayor capo");
-    if(edad<=17)
-        alert("Sos menor");
+//     let edad = prompt("Cuantos años tenes?")
+//     if (edad>=18)
+//         alert("Sos mayor capo");
+//     if(edad<=17)
+//         alert("Sos menor");
         
-    alert("Y, para la consigna, vos a hacer la tabla de edad por el numero que desees")
-    alert("La cantidad de veces que desees c:")
+//     alert("Y, para la consigna, vos a hacer la tabla de edad por el numero que desees")
+//     alert("La cantidad de veces que desees c:")
 
-    alert("Tienes " + edad)
+//     alert("Tienes " + edad)
 
-    const hasta = prompt("Cuanta cantidad de tablas?")
+//     const hasta = prompt("Cuanta cantidad de tablas?")
 
-    for(let i = 1; i <= hasta; i++) {
-        const resultado = i * edad;
-        alert(`${edad} * ${i} = ${resultado}`);}
+//     for(let i = 1; i <= hasta; i++) {
+//         const resultado = i * edad;
+//         alert(`${edad} * ${i} = ${resultado}`);}
     
-}
+// }
+
+const
+nombres = ["Martin" ,"Julieta", "Jorge" , "Tomas", "Mauro", "Andres"]
+$submit = document.getElementById("submit"),
+$password = document.getElementById("password"),
+$username = document.getElementById("username"),
+$visible = document.getElementById("visible");
+
+document.addEventListener("change", (e)=>{
+    if(e.target === $visible){
+        if($visible.checked === false) $password.type = "password";
+        else $password.type = "text";
+    }
+});
+
+document.addEventListener("click", (e)=>{
+    if(e.target === $submit){
+        if($password.value !== "" && $username.value !== ""){
+            e.preventDefault();
+            window.location.href = "home.html"
+        }
+    }
+})
+
+function print() {
+    document.querySelector('#resultado').textContent = nombres.join(', ');
+  }
+  
