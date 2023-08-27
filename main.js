9a37e1974e9e0a022ffc2e2e4f8abef7d2a7c606
Martin Sallies <martinsalliesopkk@gmail.com>
2023-08-27 06:08:37 -0300

    3ra preentrega

--- main.js
+++ main.js
@@ -1,31 +1,35 @@
-var cuentas = [ 
-    {
-        username: "Martin",
-        password: "12345"
-    },
-    {
-        username: "Jorge",
-        password: "123" 
-    },
-    {
-        username: "Julieta",
-        password: "mas123"
-    },
-    {
-        username: "Mauro",
-        password: "sheros"
-    },
-]
+let users = []
 
-function getInfo() {
-    var username = document.getElementById("username").value
-    var password = document.getElementById("password").value
-
-    for(i = 0; i < cuentas.length; i++){
-        if(username == cuentas[i].username && password == cuentas[i].password){
-            window.location.href = "home.html"
-        return
-    }
-  }
-  alert("Contraseña y/o usuario incorrecto")
+if(localStorage.getItem("user")){
+    let usersArray = JSON.parse(localStorage.getItem("user")) || []
+    
+    console.log(typeof usersArray)
 }
+
+let form = document.getElementById("form")
+
+if(localStorage.getItem("username") && localStorage.getItem("password")){
+    document.getElementById("username").value = localStorage.getItem("username")
+    document.getElementById("password").value = localStorage.getItem("password")
+}
+
+
+
+form.addEventListener("submit", (e) =>{
+    e.preventDefault()
+    let username = document.getElementById("username").value
+    let password = document.getElementById("password").value
+
+    let user = {
+        username:username,
+        password:password,
+    }
+    // storage
+
+    localStorage.setItem("user",JSON.stringify(user))
+
+    localStorage.setItem("username", username)
+    localStorage.setItem("password", password)
+
+    alert("Detalles guardados en los archivos")
+})
